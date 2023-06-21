3df84f82ff9fee0193173eaba5760684182a4bf1
ArtsemKalitsenia <133810963+DonKalishman@users.noreply.github.com>
2023-06-21 13:41:31 +0300

    File_09

---
+++ Assets/Test task/Scripts/AnimatorControllerHand.cs
@@ -0,0 +1,31 @@
+//#region import
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using System;
+//#endregion
+
+public class AnimatorControllerHand : MonoBehaviour
+{
+    //#region editors fields and properties
+    //#endregion
+
+    //#region public fields and properties
+    //#endregion
+
+    //#region private fields and properties
+    //#endregion
+
+
+    //#region life-cycle callbacks
+    //#endregion
+
+    //#region public methods
+    //#endregion
+
+    //#region private methods
+    //#endregion
+
+    //#region event handlers
+    //#endregion
+}
